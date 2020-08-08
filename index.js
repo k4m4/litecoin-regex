2eef4e6b420b8952af4781415d4ac4c8c23084a7
k4m4 <nikolaskam@gmail.com>
2020-08-08 19:56:33 +0300

    Fix styling issues

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 const re = '[LM3][a-km-zA-HJ-NP-Z1-9]{26,33}';
 
-module.exports = opts => {
-	opts = opts || {};
-	return opts.exact ? new RegExp('(?:^' + re + '$)') : new RegExp(re, 'g');
+module.exports = options => {
+	options = options || {};
+	return options.exact ? new RegExp('(?:^' + re + '$)') : new RegExp(re, 'g');
 };
